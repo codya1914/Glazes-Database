82d4f488173fad786000e0c52065e32416461cdb
Cody Adams <cadams24@coe.edu>
2026-06-17 15:43:23 +0000

    This is a basic understanding

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Django example",
+        "glaze_name": "Blue Ash",
+        "cone": "Cone 10",
+        "color": "Blue / Gray",
+        "surface": "Glossy",
+        "notes": "This is hard coded. A little test run"
     }
+
     return render(request, "index.html", context)
